a166339eedf0b5cbf9805e04be15e9f1060b7677
omarvegaabreu <39449747+omarvegaabreu@users.noreply.github.com>
2020-05-04 18:42:50 -0400

    dom elements

---
+++ assets/scripts/app.js
@@ -0,0 +1,66 @@
+"use strict";
+
+// /**STAYED AT 283 DOM ELEMENTS */
+
+class ToolTip {}
+
+class ProjectItem {
+  constructor(id, updateProjectListsFunction) {
+    this.id = id;
+    this.updateProjectListsHandler = updateProjectListsFunction;
+    this.connectMoreInfoButton();
+    this.connectSwitchButton();
+  }
+
+  connectMoreInfoButton() {}
+  connectSwitchButton() {
+    const projectItemElement = document.getElementById(this.id);
+    const switchBtn = projectItemElement.querySelector("button:last-of-type");
+    switchBtn.addEventListener("click", this.updateProjectListsHandler); //calls switchProject(fn)
+  }
+}
+
+class ProjectList {
+  projects = [];
+  //receives project list from dom
+  constructor(type) {
+    this.type = type;
+    const prjItems = document.querySelectorAll(`#${type}-projects li`);
+    for (const prjItem of prjItems) {
+      this.projects.push(
+        new ProjectItem(prjItem.id, this.switchProject.bind(this))
+      );
+    }
+    console.log(this.projects);
+  }
+
+  setSwitchHandlerFunction(switchHandlerFunction) {
+    this.switchHandler = switchHandlerFunction;
+  }
+
+  addProject(project) {
+    this.projects.push(project);
+  }
+
+  switchProject(projectId) {
+    // const projectIndex = this.projects.findIndex(p => p.id === projectId);
+    // this.projects.splice(projectIndex, 1);
+    this.switchHandler(this.projects.find((p) => p.id === projectId));
+    this.projects = this.projects.filter((p) => p.id !== projectId);
+  }
+}
+
+class App {
+  static init() {
+    const activeProjectList = new ProjectList("active");
+    const finishedProjectList = new ProjectList("finished");
+    activeProjectList.setSwitchHandlerFunction(
+      finishedProjectList.addProject.bind(finishedProjectList)
+    );
+    finishedProjectList.setSwitchHandlerFunction(
+      activeProjectList.addProject.bind(activeProjectList)
+    );
+  }
+}
+
+App.init();
